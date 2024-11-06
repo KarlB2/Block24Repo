0102ef56bdebe562f04512162b01e79f1b7bef61
KarlB2 <karlbusse242@gmail.com>
2024-11-05 19:44:32 -0600

    Finished Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
+  function handleClick() {
+    //something
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {
 
+        puppies.map((puppy) => {
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
